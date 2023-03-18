df5612b3b4e3249f115b0fd3d41c672261be91f8
DawitDir <dawitccnt@gmail.com>
2023-03-18 21:48:31 +0300

    routes

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const app = express();
+const bodyParse = require("body-parser");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const serverless = require("serverless-http");
+require("dotenv").config();
+app.use(bodyParse.json());
+app.use(cookieParser());
+app.use(bodyParse.urlencoded({ extended: true }));
+
+// cors
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 app.use("/api", require("./src/routes/routes"));
 
-app.get("/api", (req, res) => {
-  res.send({ api: "api" });
-});
+app.use(express.static("assets"));
+
+mongoose
+  .connect(process.env.MONGODB_API_KEY, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("db connected"))
+  .catch((err) => console.log(err));
 app.listen(process.env.PORT || 3000);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,17 @@
 const { response } = require("express");
 const express = require("express");
 const router = express.Router();
-// const User = require("../models/User");
-// const Song = require("../models/Song");
 // const authController = require("../controller/authController");
-// const SongController = require("../controllers/SongController");
+const SongController = require("../controllers/SongController");
 
 router.get("/", (req, res) => {
   res.send({
     status: "success",
+  });
+});
+router.get("/hello", (req, res) => {
+  res.send({
+    hello: "mother",
   });
 });
 // router.post("/signup", authController.singup_post);
@@ -16,10 +19,10 @@ router.get("/", (req, res) => {
 // router.post("/verifyToken", authController.verifyToken);
 // router.get("/logout", authController.logout_user);
 
-// router.post("/songs", SongController.store);
-// router.get("/songs", SongController.index);
-// router.get("/songs/:id", SongController.fetchSingleSong);
-// router.delete("/songs/:id", SongController.destroy);
-// router.put("/songs/:id", SongController.update);
-// router.put("/update/:id", SongController.updateWithImage);
+router.post("/songs", SongController.store);
+router.get("/songs", SongController.index);
+router.get("/songs/:id", SongController.fetchSingleSong);
+router.delete("/songs/:id", SongController.destroy);
+router.put("/songs/:id", SongController.update);
+router.put("/update/:id", SongController.updateWithImage);
 module.exports = router;
